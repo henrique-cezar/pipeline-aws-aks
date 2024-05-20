a83495be3489e7bc2c677f2bd7fc3976b717c46a
Henrique Cezar <henriquecezar.estudo+aws1@gmail.com>
2024-05-19 22:15:45 -0300

    Alterações sobre tags para AKS

--- src/main.tf
+++ src/main.tf
@@ -12,32 +12,43 @@ module "vpc" {
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
-  tags = var.aws_project_tags
+  tags = merge(var.aws_project_tags, {"kubernetes.io/cluster/${var.aws_eks_name}" = "shared"})
+
+  #Tags needed to allow AKS to use the VPC
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
+    "kubernetes.io/roles/elb" = 1
+  }
+  
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
+    "kubernetes.io/roles/internal-elb" = 1
+  }
 
 }
 
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "20.10.0"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.10.0"
 
-#   cluster_name    = var.aws_eks_name
-#   cluster_version = var.aws_eks_version
+  cluster_name    = var.aws_eks_name
+  cluster_version = var.aws_eks_version
 
-#   enable_cluster_creator_admin_permissions = true
+  enable_cluster_creator_admin_permissions = true
 
-#   ## subnets
-#   subnet_ids                     = module.vpc.private_subnets
-#   vpc_id                         = module.vpc.vpc_id
-#   cluster_endpoint_public_access = true
-#   eks_managed_node_groups = {
-#     default = {
-#       min_size     = 2
-#       max_size     = 2
-#       desired_size = 2
+  ## subnets
+  subnet_ids                     = module.vpc.private_subnets
+  vpc_id                         = module.vpc.vpc_id
+  cluster_endpoint_public_access = true
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
 
-#       instance_types = var.aws_eks_managed_node_groups_instance_types
-#     }
-#   }
-# }
+      instance_types = var.aws_eks_managed_node_groups_instance_types
+    }
+  }
+}
 
 #Parei em 1:49:09
